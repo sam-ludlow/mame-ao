3d6c3f7ca1f751c3ceda687ba27efef79bc2964a
sam-ludlow <sam@spludlow.co.uk>
2025-10-07 15:29:09 +0100

    fix AssemblyVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,15 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e31cc6e2-c749-4cb7-b4a3-78cc1da1f222")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.133")]
 [assembly: AssemblyFileVersion("1.133")]
